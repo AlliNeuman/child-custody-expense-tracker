80569948d016544d0a1afb160ad2027232b9f407
Alli Tzadok <allisontzadok@gmail.com>
2018-10-23 16:42:02 -0500

    add skeleton methods in expenses and categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,42 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :show, :update, :destroy]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_category
+
+  end
+
+  def category_params
+
+  end
+
 end
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,41 @@
 class ExpensesController < ApplicationController
+  before_action :set_expense, only: [:edit, :show, :update, :destroy]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_expense
+
+  end
+
+  def expense_params
+
+  end
 end
