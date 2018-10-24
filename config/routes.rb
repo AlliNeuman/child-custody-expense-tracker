4a84682bf6bc536d8740768b2abc7f94bee885bc
Alli Tzadok <allisontzadok@gmail.com>
2018-10-24 15:02:40 -0500

    update categories resources to include update and destroy routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :expenses, only: [:index, :show, :create, :update, :destroy]
-  resources :categories, only: [:index, :show, :create]
-
+  resources :categories, only: [:index, :show, :create, :update, :destroy]
 end
