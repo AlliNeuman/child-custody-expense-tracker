e0f2c70934c6915554a99b8524e2d7d791086d22
Alli Tzadok <allisontzadok@gmail.com>
2018-10-23 22:18:08 -0500

    add validations to models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,8 @@
 class Expense < ApplicationRecord
   belongs_to :category
+  validates :date, presence: true
+  validates :amount, presence: true
+  validates :description, presence: true
+  validates :reimburse_percent, presence: true
+  validates :category_id, presence: true 
 end
