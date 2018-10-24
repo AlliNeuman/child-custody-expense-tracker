a023a45928844af12ef37f58841ec22f87a397bf
Alli Tzadok <allisontzadok@gmail.com>
2018-10-23 21:40:05 -0500

    add methods to expenses controller for regular index, show, and create methods, and private methods

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,15 +2,17 @@ class ExpensesController < ApplicationController
   before_action :set_expense, only: [:edit, :show, :update, :destroy]
 
   def index
-
+    @expenses = Expense.all
+    # need something to order by date desc
+    render json: @expenses
   end
 
   def show
-
+    # need render json stuff here
+    render json: @expense
   end
 
   def new
-
   end
 
   def edit
@@ -18,7 +20,14 @@ class ExpensesController < ApplicationController
   end
 
   def create
-
+    @expense = Expense.new(expense_params)
+    if @expense.valid?
+      @expense.save
+      @expenses = Expense.order(date: :desc)
+      render json: @expenses
+    else
+      render json: { message: "Something went wrong creating your new record. Please try again."}
+    end
   end
 
   def update
@@ -32,10 +41,10 @@ class ExpensesController < ApplicationController
   private
 
   def set_expense
-
+    @expense = Expense.find(params[:id])
   end
 
   def expense_params
-
+    params.require(:expense).permit(:date, :amount, :description, :reimburse_percent, :paid, :category_id)
   end
 end
