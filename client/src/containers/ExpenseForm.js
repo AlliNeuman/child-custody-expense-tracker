5f6f90925e313d4afdefb258a6364b81a8757028
Alli Tzadok <allisontzadok@gmail.com>
2018-11-05 13:05:41 -0600

    add constructor method and state initializer to expenseForm.js

--- client/src/containers/ExpenseForm.js
+++ client/src/containers/ExpenseForm.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class ExpenseForm extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      date: '',
+      category: '',
+      description: '',
+      amount: '',
+      reimbursePercent: '',
+      expensePaid: ''
+    }
+  }
+
+  render() {
+    return (
+
+    )
+  }
 
 }
 
