ba7be96a28183d971d7dd383c517f41e517f57df
Alli Tzadok <allisontzadok@gmail.com>
2018-11-05 17:55:17 -0600

    fix store setup due to only allowing 2 arguments in createStore

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,11 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers/index';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+
 const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
+  rootReducer, composeEnhancer(applyMiddleware(thunk)),
 );
 
 export default store;
