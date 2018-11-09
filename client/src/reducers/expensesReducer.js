d3829d27bbd0c71559bb7fc0a05c103cdddd1671
Alli Tzadok <allisontzadok@gmail.com>
2018-11-09 12:31:34 -0600

    add expense reducer switch function

--- client/src/reducers/expensesReducer.js
+++ client/src/reducers/expensesReducer.js
@@ -1,3 +1,30 @@
 export default (state=[], action) => {
+  switch(action.type) {
+    case 'LOADING_EXPENSES':
+      return state
 
+    case 'FETCH_EXPENSES':
+      return action.expenses
+
+    case 'FETCH_EXPENSE':
+      return action.expense
+
+    case 'ADD_EXPENSE':
+      return state.concat(action.expense);
+
+    case 'REMOVE_EXPENSE':
+      return state.filter(expense => expense.id !== action.id)
+
+    case 'UPDATE_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.expense.id) {
+          return action.expense
+        } else {
+          return expense
+        }
+      });
+
+    default:
+      return state;
+  }
 }
