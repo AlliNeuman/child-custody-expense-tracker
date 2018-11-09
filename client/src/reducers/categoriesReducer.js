605989d5d7f47f87ddb6f47509f85114da73d3eb
Alli Tzadok <allisontzadok@gmail.com>
2018-11-09 12:35:57 -0600

    add category reducer switch function

--- client/src/reducers/categoriesReducer.js
+++ client/src/reducers/categoriesReducer.js
@@ -1,3 +1,20 @@
 export default (state=[], action) => {
 
+  switch(action.type) {
+
+    case 'LOADING_CATEGORIES':
+      return state
+
+    case 'FETCH_CATEGORIES':
+      return action.categories
+
+    case 'FETCH_CATEGORY':
+      return action.category
+
+    case 'ADD_CATEGORY':
+      return state.concat(action.category);
+
+    default:
+      return state;
+  }
 }
