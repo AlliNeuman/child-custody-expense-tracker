ee57e2b526740f92286508b8d687226141e2d4ab
Alli Tzadok <allisontzadok@gmail.com>
2018-11-09 13:07:02 -0600

    add mapping for expenseList and returning the list group

--- client/src/components/ExpenseList.js
+++ client/src/components/ExpenseList.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ExpenseListItem from 'react';
+import { ListGroup } from 'react-bootstrap';
 
-const ExpenseList = (props) => {
-  if (props) {
-    const expenses = props.expenses.map(expense => <ExpenseListItem key={expense.id} expense={expense} /> )
-    return (
-        <div className="expense-list">
+const ExpenseList = ({ expenses }) => {
+  const renderExpenses = expenses.map(expense =>
+    <ExpenseListItem key={expense.id} expense={expense} />
+  );
 
-        </div>
-    )
-  }
+  return (
+    <div className="expense-list">
+    <ListGroup>
+      {renderExpenses}
+    </ListGroup>
+    </div>
+  )
 }
+
 
 export default ExpenseList;
