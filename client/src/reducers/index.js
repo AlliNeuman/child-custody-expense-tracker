45586171472a5e2e612b00db7e54df74b2cd8ef2
Alli Tzadok <allisontzadok@gmail.com>
2018-11-09 11:52:08 -0600

    remove reducers that were removed from the index.js reducer file

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,9 @@
 import { combineReducers } from  'redux';
 import expensesReducer from './expensesReducer';
-import expenseReducer from './expenseReducer';
 import categoriesReducer from './categoriesReducer';
-import categoryReducer from './categoryReducer';
 
 
 export const rootReducer = combineReducers({
   expenses: expensesReducer,
-  expense: expenseReducer,
-  categories: categoriesReducer,
-  category: categoryReducer
+  categories: categoriesReducer
 })
