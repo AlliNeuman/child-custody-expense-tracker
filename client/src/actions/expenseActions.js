0ef18b47311e9ec28d1149fed55bc3d918c4623c
Alli Tzadok <allisontzadok@gmail.com>
2018-11-09 12:26:00 -0600

    refactor expense actions

--- client/src/actions/expenseActions.js
+++ client/src/actions/expenseActions.js
@@ -1,32 +1,90 @@
 import fetch from 'cross-fetch';
 import { API_URL } from './APIurl';
 
-export function fetchExpenses() {
-  return (dispatch) => {
-    dispatch({ type: "LOADING_EXPENSES"});
-
-    const expensesUrl = `${API_URL}/expenses`
-
-    return fetch(expensesUrl, {method: 'get'})
-      .then(response => response.json())
-      .then(expenses => dispatch({
-        type: 'FETCH_EXPENSES',
-        payload: expenses}))
-      .catch(error => console.error(error))
-  };
+const setExpenses = expenses => {
+  return {
+    type: 'FETCH_EXPENSES',
+    expenses
+  }
 }
 
-export function fetchExpense(expense){
-  return (dispatch) => {
-    dispatch({ type: "LOADING_EXPENSE" });
+const setExpense = expense => {
+  return {
+    type: 'FETCH_EXPENSE',
+    expense
+  }
+}
 
-    const expenseUrl = `${API_URL}/expenses/${expense.expenseId}`
+const addExpense = expense => {
+  return {
+    type: 'ADD_EXPENSE',
+    expense
+  }
+}
 
-    return fetch(expenseUrl, {method: 'get'})
-      .then(response => response.json())
-      .then(expense => dispatch({
-        type: 'LOADING_EXPENSE',
-        payload: expense}))
-      .catch(error => console.error(error))
-  };
+const updateExpense = expense => {
+  return {
+    type: 'UPDATE_EXPENSE',
+    expense
+  }
+}
+
+const deleteExpense = expense => {
+  return {
+    type: 'DELETE_EXPENSE',
+    expense
+  }
+}
+
+export const fetchExpenses = () => {
+  return dispatch => {
+    dispatch({type: 'LOADING_EXPENSES'});
+    return fetch(`${API_URL}/expenses`)
+    .then(response => response.json())
+    .then(expenses => dispatch(setExpenses(expenses)))
+    .catch(error => console.error(error))
+  }
+}
+
+export const fetchExpense = () => {
+  return dispatch => {
+    return fetch(`${API_URL}/expenses/${expenseId}`)
+    .then(response => response.json())
+    .then(expense => {
+      dispatch(setExpense([expense]));
+    })
+    .catch(error => console.error(error))
+  }
+}
+
+export const createExpense = (expense) => {
+  return dispatch => {
+    return fetch(`${API_URL}/expenses`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({expense: expense})
+    })
+    .then(response => response.json())
+    .then(expense => {
+      dispatch(addExpense(expense))
+    })
+    .catch(error => {
+      dispatch({type: 'error'})
+    })
+  }
+}
+
+export const deleteExpense = (expenseId, routerHistory) => {
+  return dispatch => {
+    return fetch(`${API_URL}/expenses/${expenseId}`, {
+      method: "DELETE",
+    })
+    .then(response => {
+      dispatch(deleteExpense(expenseId));
+      routerHistory.replace('/expenses')
+    })
+    .catch(error => console.error(error))
+  }
 }
