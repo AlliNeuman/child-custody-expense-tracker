fb4ccf1339b9cd71509f884b0f046995695db497
Alli Tzadok <allisontzadok@gmail.com>
2018-11-09 12:09:17 -0600

    refactor category actions

--- client/src/actions/categoryActions.js
+++ client/src/actions/categoryActions.js
@@ -1,33 +1,62 @@
 import fetch from 'cross-fetch';
 import { API_URL } from './APIurl';
 
-export function fetchCategories() {
-  return (dispatch) => {
-    dispatch({ type: "LOADING_CATEGORIES"});
-
-    const categoriesUrl = `${API_URL}/categories`
-
-    return fetch(categoriesUrl, {method: 'get'})
-      .then(response => response.json())
-      .then(categories => dispatch({
-        type: 'FETCH_CATEGORIES',
-        payload: categories
-      }));
-  };
+const setCategories = categories => {
+  return {
+    type: 'FETCH_CATEGORIES',
+    categories
+  }
 }
 
+const setCategory = category => {
+  return {
+    type: 'FETCH_CATEGORY',
+    category
+  }
+}
 
-export function fetchCategory(category){
-  return (dispatch) => {
-    dispatch({ type: "LOADING_CATEGORY" });
+const addCategory = category => {
+  return {
+    type: 'ADD_CATEGORY',
+    category
+  }
+}
 
-    const categoryUrl = `${API_URL}/categories/${category.categoryId}`
-
-    return fetch(categoryUrl, {method: 'get'})
+export const fetchCategories = () => {
+  return dispatch => {
+    dispatch({type: 'LOADING_CATEGORIES'});
+      return fetch(`${API_URL}/categories`)
       .then(response => response.json())
-      .then(category => {dispatch({
-        type: 'LOADING_CATEGORY',
-        payload: category
-      })});
-  };
+      .then(categories => dispatch(setCategories(categories)))
+      .catch(error => console.error(error))
+  }
+}
+
+export const fetchCategory = categoryId => {
+  return dispatch => {
+    return fetch(`${API_URL}/categories/${categoryId}`)
+    .then(response => response.json())
+    .then(category => {dispatch(setCategory([category]));
+    })
+    .catch(error => console.error(error))
+  }
+}
+
+export const createCategory = (category) => {
+  return dispatch => {
+    return fetch(`${API_URL}/categories`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({category: category})
+    })
+    .then(response => response.json())
+    .then(category => {
+      dispatch(addCategory(category))
+    })
+    .catch(error => {
+      dispatch({type: 'error'})
+    })
+  }
 }
