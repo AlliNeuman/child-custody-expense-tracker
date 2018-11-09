6d3e49606bfec541127346a72e4d875e943043d5
Alli Tzadok <allisontzadok@gmail.com>
2018-11-09 14:06:11 -0600

    add constructor, handle on change and handle on submit for expense form

--- client/src/containers/ExpenseForm.js
+++ client/src/containers/ExpenseForm.js
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { createExpense } from '../actions/expenseActions';
+import { FormControl, FormGroup } from 'react-bootstrap';
 
 class ExpenseForm extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      date: '',
+      category: '',
+      description: '',
+      amount: '',
+      reimburse: '',
+      paid: ''
+    };
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.props.createExpense(this.state)
+    this.props.history.push('/expenses')
+  }
+
   render() {
     return (
-      <form>
+      <div className="expense-form">
+      <h4>Add an Expense</h4>
+      <form onSubmit={this.handleOnSubmit}>
 
-      </form>
     )
   }
 }
