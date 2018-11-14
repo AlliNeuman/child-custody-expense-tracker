b30345483544fcc0ada693beb2a210e7b63605cc
Alli Tzadok <allisontzadok@gmail.com>
2018-11-14 13:30:48 -0600

    add onSubmit and onChange functions for expenseNew and skeleton for mapStateToProps and mapDispatchToProps

--- client/src/containers/ExpenseNew.js
+++ client/src/containers/ExpenseNew.js
@@ -18,5 +18,46 @@ class ExpenseNew extends Component {
     };
   }
 
+  handleOnSubmit = event => {
+    event.preventDefault();
+    const { createExpense, history } = this.props
 
+    createExpense({
+      expense: {
+        date: this.state.date,
+        category: this.state.category,
+        description: this.state.description,
+        amount: this.state.amount,
+        reimburse: this.state.reimburse,
+        obligation: this.state.obligation,
+        paid: this.state.paid,
+      }
+    });
+    history.push('/expenses');
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+      <h3>Add a New Expense</h3>
+      <ExpenseForm handleOnChange={this.handleOnChange} handleOnSubmit={this.handleOnSubmit} />
+      </div>
+    )
+  }
 }
+
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+  
+})
+
+export default ExpenseNew;
