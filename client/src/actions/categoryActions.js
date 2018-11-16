56823a4357eea40a36eeb3fdff3179bb30edeb72
Alli Tzadok <allisontzadok@gmail.com>
2018-11-16 13:02:04 -0600

    add fetchExpenses and createExpense actions

--- client/src/actions/categoryActions.js
+++ client/src/actions/categoryActions.js
@@ -1,13 +1,13 @@
-import { API_URL } from './APIurl';
+import fetch from 'cross-fetch';
 
 export function fetchCategories() {
   return (dispatch) => {
     dispatch({type: 'LOADING_CATEGORIES'})
-    return fetch(`${API_URL}/categories`)
+    return fetch('http://localhost:3001/categories')
     .then(response => response.json())
     .then(categories => dispatch({
       type: "FETCH_CATEGORIES",
-      categories: categories}))
+      payload: categories}))
 
     .catch(error => console.error(error))
   }
--- client/src/actions/expenseActions.js
+++ client/src/actions/expenseActions.js
@@ -1,1 +1,29 @@
-// need a createExpense
+import fetch from 'cross-fetch';
+
+export function fetchExpenses() {
+  return (dispatch) => {
+    dispatch({type: 'LOADING_EXPENSES'})
+    return fetch('http://localhost:3001/expenses')
+    .then(response => response.json())
+    .then(expenses => dispatch({
+      type: "FETCH_EXPENSES",
+      payload: expenses}))
+
+    .catch(error => console.error(error))
+  }
+}
+
+export function createExpense(formContent) {
+  return (dispatch) => {
+    dispatch({ type: 'ADD_EXPENSE'})
+    return fetch('http://localhost:3001/expenses', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(formContent)})
+      .then(response => response.json())
+      .then(expenses => dispatch({
+        type: "UPDATE_EXPENSE",
+        payload: expenses}))
+      .catch(error => console.error(error))
+  }
+}
