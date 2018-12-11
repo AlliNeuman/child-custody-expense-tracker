b5b417139aaec193d654359554add5252ef0b4c3
Alli Tzadok <allisontzadok@gmail.com>
2018-12-10 22:10:25 -0600

    add button and expensePaid for parent obligation column. need to tie into state to keep track

--- client/src/components/ExpenseListItem.js
+++ client/src/components/ExpenseListItem.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {Button} from 'react-bootstrap';
-import PaidButton from './PaidButton';
-// import PaidAmount from './PaidAmount';
+
+
 
 class ExpenseListItem extends Component {
 
@@ -16,62 +16,64 @@ class ExpenseListItem extends Component {
       description: props.description,
       amount: props.amount,
       reimburse_percent: props.reimburse_percent,
-      paid: props.paid
+      paid: props.paid,
+      parentObligation: parseFloat(props.amount * props.reimburse_percent).toFixed(2)
+    }
+    this.handlePaidClick = this.handlePaidClick.bind(this);
+    }
+
+    handlePaidClick = () => {
+      this.setState( state => ({
+        paid: !state.paid
+      }));
+    }
+
+    render() {
+      const paidObligation = parseFloat((-1)*this.state.parentObligation).toFixed(2);
+
+      return (
+        <React.Fragment>
+        <tr>
+        <td className="col mx-1 px-3">
+        <Link className="text-dark" to={`/expenses/${this.state.id}`}>
+        {this.state.date}
+        </Link>
+        </td>
+
+        <td className="col mx-1 px-3">
+        {this.state.category}
+        </td>
+
+        <td className="col mx-1 px-3">
+        {this.state.description}
+        </td>
+
+        <td className="col mx-1 px-3">
+        ${this.state.amount}
+        </td>
+
+        <td className="col mx-1 px-3">
+        {this.state.reimburse_percent}
+        </td>
+
+        <td>
+        ${this.state.parentObligation}
+        </td>
+
+        <td>
+        ${this.state.paid ? paidObligation : this.state.parentObligation}
+        </td>
+
+        <td>
+        <Button onClick={this.handlePaidClick}>
+        {this.state.paid ? 'Not Paid' : 'Paid'}
+        </Button>
+        </td>
+
+        </tr>
+        </React.Fragment>
+      )
     }
   }
 
-  render() {
-    const a = this.state.amount;
-    const r = this.state.reimburse_percent
-    const parentObligation = parseFloat(a*r).toFixed(2)
-    // debugger
-
-    const p = parseFloat(parentObligation*(-1)).toFixed(2)
-
-
-    return (
-      <React.Fragment>
-      <tr>
-        <td className="col mx-1 px-3">
-          <Link className="text-dark" to={`/expenses/${this.state.id}`}>
-          {this.state.date}
-          </Link>
-        </td>
-
-        <td className="col mx-1 px-3">
-          {this.state.category}
-        </td>
-
-        <td className="col mx-1 px-3">
-          {this.state.description}
-        </td>
-
-        <td className="col mx-1 px-3">
-          ${this.state.amount}
-        </td>
-
-        <td className="col mx-1 px-3">
-          {this.state.reimburse_percent}
-        </td>
-        <td>
-        ${parentObligation}
-        </td>
-
-        <td>
-
-        </td>
-
-        <td>
-        <PaidButton paid={this.state.paid}/>
-        </td>
-
-
-
-
-        </tr>
-    </React.Fragment>
-    )
-  }
-}
-
-export default ExpenseListItem;
+  export default ExpenseListItem;
