4fb35da5742058a3fdc2b1b95fd693d75cc8f80f
AlliNeuman <alli.neuman@gmail.com>
2019-03-04 23:09:13 -0600

    update application controller to fix index load error issues for potential loading

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :expenses, only: [:index, :show, :create, :update, :destroy]
   resources :categories, only: [:index, :show, :create, :update, :destroy]
 
+get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  !request.xhr? && request.format.html?
+end
 end
