adbda96116cf357f9a8fd62720d6226c245bc604
AlliNeuman <alli.neuman@gmail.com>
2019-03-04 23:57:44 -0600

    update skip before action in application controller to try to fix error for app crashing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::API
   protect_from_forgery unless: -> {request.format.json?}
+  skip_before_action :verify_authenticity_token, raise: false
 
     def fallback_index_html
     render :file => 'public/index.html'
-  end
+    end
 end
