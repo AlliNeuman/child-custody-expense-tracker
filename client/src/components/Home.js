7d0996b9cc79cc5c01bd5394eafa7975fe2c3ffc
AlliNeuman <alli.neuman@gmail.com>
2019-03-04 20:11:55 -0600

    update home screen with photo and extra button for expenses in jumbotron styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import '../App.css';
 
 export const Home = () =>
-  <div className="container">
-    <h2 className="WelcomeMessage">Welcome to YOM!</h2>
-    <p className="HomeDescription">Need help tracking your childcare expenses for your spouse that drags their feet and never pays? This will help you keep track so you do not lose your mind!</p>
-  </div>
+<div className="container center">
+  <h2 className="WelcomeMessage">Welcome to YOM!</h2>
+  <p className="HomeDescription">Need help tracking your childcare expenses? This will help you keep track of outstanding and paid expenses so you do not lose your mind!</p>
+  <div className="col-xs-12">
+    <div className="jumbotron center you-owe-me">
+
+      <Link to="/expenses">
+        <button type="button" className="btn btn-lg btn-primary">You Owe Me
+        </button>
+      </Link>
+    </div>
+
+    </div>
+</div>
--- client/src/components/Totals.js
+++ client/src/components/Totals.js
@@ -3,10 +3,12 @@ import React from 'react';
 const Totals = (props) => {
 // debugger
   return (
+    <div className="totals">
     <div className="row">
     <div className="card card-body warning" id="outstanding-expenses">
     <h3 className="text-dark">Outstanding Expenses</h3>
     <h5>Total Unpaid: ${props.outstandingTotal}</h5>
+    </div>
     </div>
     </div>
   )
